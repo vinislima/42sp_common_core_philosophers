0dd3a5b459aa4291f086de74ef5a5653e919c923
Vinicius Da Silva Lima <vinda-si@c1r4p1.42sp.org.br>
2025-07-21 19:45:31 -0300

    context

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: vinda-si <vinda-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/18 22:18:09 by vinda-si          #+#    #+#             */
-/*   Updated: 2025/07/20 14:58:02 by vinda-si         ###   ########.fr       */
+/*   Updated: 2025/07/21 19:32:55 by vinda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,16 +41,28 @@ void	acquire_forks(t_philosopher *philo)
 	int	left_index;
 	int	right_index;
 
+	if (check_simulation_end(philo->simulation))
+		return ;
 	left_index = philo->id - 1;
 	right_index = philo->id % philo->simulation->num_philosophers;
 	if (left_index < right_index)
 	{
 		take_left_fork(philo);
+		if (check_simulation_end(philo->simulation))
+		{
+			pthread_mutex_unlock(philo->left_fork);
+			return ;
+		}
 		take_right_fork(philo);
 	}
 	else
 	{
 		take_right_fork(philo);
+		if (check_simulation_end(philo->simulation))
+		{
+			pthread_mutex_unlock(philo->right_fork);
+			return ;
+		}
 		take_left_fork(philo);
 	}
 }
